f577822033a3c37c91e66d1813074db57031246a
DmitriyRya <111747417+DmitriyRya@users.noreply.github.com>
2022-09-05 23:04:44 +0200

    Main structure, basic db and etc.

---
+++ Champions/app/app/champion/ChampionControler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Champions.Source_codes.Champions
+{
+    public class ChampionControler
+    {
+        public ChampionControler()
+        {
+
+        }
+    }
+}
